263d658afa5a41e5be873d249c60fd2180f5004e
dasbd72 <twbd723@gmail.com>
2025-04-02 12:34:53 +0800

    2873

---
+++ problems/2873-maximum-value-of-an-ordered-triplet-i/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    long long maximumTripletValue(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    ans = max((long long)(nums[i] - nums[j]) * nums[k], ans);
+                }
+            }
+        }
+        return ans;
+    }
+};
